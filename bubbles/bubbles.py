1a3a63156fb38712ed87f343139950c6f4d49f8d
rodrigo <rodrigomatiasxavier@gmail.com>
2020-08-19 19:00:36 -0800

    performing algorithm

---
+++ bubbles/bubbles.py
@@ -0,0 +1,271 @@
+import pygame
+from pygame.locals import *
+import random, math, sys
+import time
+import cv2
+import numpy as np
+import os
+
+
+# Configurations
+IMG_PATH = "../../.database/pibic/pygame/img_test/"
+NPZ_PATH = "../../.database/pibic/pygame/npz_test/"
+NUMBER_OF_DATA = 100
+FPS = 300
+CIRCLE_BUBBLES = 1
+SQUARE_BUBBLES = 0
+BUBBLES_COLOR = (255,255,255)
+SURFACE_COLOR = (0,0,0)
+WIDTH, HEIGHT = 50, 50
+BUBBLES_RADIUS = 5
+
+# Init Game
+pygame.init()
+
+# Set game configurations
+surface = pygame.display.set_mode((WIDTH,HEIGHT))
+clock = pygame.time.Clock()
+
+
+class Circle:
+    def __init__(self):
+        self.color = BUBBLES_COLOR
+        self.radius = BUBBLES_RADIUS
+        self.x = random.randint(self.radius+1, WIDTH-self.radius-1)
+        self.y = random.randint(self.radius+1, HEIGHT-self.radius-1)
+        self.speedx = random.random()
+        self.speedy = random.random()
+    
+    def board_collision(self):
+        if self.x < self.radius or self.x > WIDTH-self.radius:
+            self.speedx *= -1
+        if self.y < self.radius or self.y > HEIGHT-self.radius:
+            self.speedy *= -1
+    
+    def circle_collision(self, other_circle):
+        if math.sqrt(((self.x-other_circle.x)**2)+((self.y-other_circle.y)**2)) <= (self.radius+other_circle.radius):
+            self.collision(other_circle)
+    
+    # def collision(self):
+    #     self.speedx *= -1
+    #     self.speedy *= -1
+
+    def collision(self, other_circle):
+        speed = math.sqrt((self.speedx**2)+(self.speedy**2))
+        diffx = -(self.x-other_circle.x)
+        diffy = -(self.y-other_circle.y)
+        if diffx > 0:
+            if diffy > 0:
+                angle = math.degrees(math.atan(diffy/diffx))
+                speedx = -speed*math.cos(math.radians(angle))
+                speedy = -speed*math.sin(math.radians(angle))
+            elif diffy < 0:
+                angle = math.degrees(math.atan(diffy/diffx))
+                speedx = -speed*math.cos(math.radians(angle))
+                speedy = -speed*math.sin(math.radians(angle))
+        elif diffx < 0:
+            if diffy > 0:
+                angle = 180 + math.degrees(math.atan(diffy/diffx))
+                speedx = -speed*math.cos(math.radians(angle))
+                speedy = -speed*math.sin(math.radians(angle))
+            elif diffy < 0:
+                angle = -180 + math.degrees(math.atan(diffy/diffx))
+                speedx = -speed*math.cos(math.radians(angle))
+                speedy = -speed*math.sin(math.radians(angle))
+        elif diffx == 0:
+            if diffy > 0:
+                angle = -90
+            else:
+                angle = 90
+            speedx = speed*math.cos(math.radians(angle))
+            speedy = speed*math.sin(math.radians(angle))
+        elif diffy == 0:
+            if diffx < 0:
+                angle = 0
+            else:
+                angle = 180
+            speedx = speed*math.cos(math.radians(angle))
+            speedy = speed*math.sin(math.radians(angle))
+        self.speedx = speedx
+        self.speedy = speedy
+    
+    def move(self):
+        self.x += self.speedx
+        self.y += self.speedy
+    
+    def show(self):
+        pygame.draw.circle(surface, BUBBLES_COLOR, (int(self.x),int(HEIGHT-self.y)), self.radius)
+
+
+class Square:
+    def __init__(self):
+        self.color = BUBBLES_COLOR
+        self.radius = BUBBLES_RADIUS # Describes the circumference that cover the square
+        self.x = random.randint(self.radius+1, WIDTH-self.radius-1)
+        self.y = random.randint(self.radius+1, HEIGHT-self.radius-1)
+        self.z = math.sqrt(2*(self.radius**2))
+        self.w = math.sqrt(2*(self.radius**2))
+        self.speedx = random.random()
+        self.speedy = random.random()
+
+        # self.radius = int((math.sqrt(2*(self.side**2))) / 2) # Describes the circumference that cover the square
+    
+    def board_collision(self):
+        if self.x < self.radius or self.x > WIDTH-self.radius:
+            self.speedx *= -1
+        if self.y < self.radius or self.y > HEIGHT-self.radius:
+            self.speedy *= -1
+    
+    def square_collision(self, other_square):
+        if math.sqrt(((self.x-other_square.x)**2)+((self.y-other_square.y)**2)) <= (self.radius+other_square.radius):
+            self.collision(other_square)
+    
+    # def collision(self):
+    #     self.speedx *= -1
+    #     self.speedy *= -1
+    
+    def collision(self, other_square):
+        speed = math.sqrt((self.speedx**2)+(self.speedy**2))
+        diffx = -(self.x-other_square.x)
+        diffy = -(self.y-other_square.y)
+        if diffx > 0:
+            if diffy > 0:
+                angle = math.degrees(math.atan(diffy/diffx))
+                speedx = -speed*math.cos(math.radians(angle))
+                speedy = -speed*math.sin(math.radians(angle))
+            elif diffy < 0:
+                angle = math.degrees(math.atan(diffy/diffx))
+                speedx = -speed*math.cos(math.radians(angle))
+                speedy = -speed*math.sin(math.radians(angle))
+        elif diffx < 0:
+            if diffy > 0:
+                angle = 180 + math.degrees(math.atan(diffy/diffx))
+                speedx = -speed*math.cos(math.radians(angle))
+                speedy = -speed*math.sin(math.radians(angle))
+            elif diffy < 0:
+                angle = -180 + math.degrees(math.atan(diffy/diffx))
+                speedx = -speed*math.cos(math.radians(angle))
+                speedy = -speed*math.sin(math.radians(angle))
+        elif diffx == 0:
+            if diffy > 0:
+                angle = -90
+            else:
+                angle = 90
+            speedx = speed*math.cos(math.radians(angle))
+            speedy = speed*math.sin(math.radians(angle))
+        elif diffy == 0:
+            if diffx < 0:
+                angle = 0
+            else:
+                angle = 180
+            speedx = speed*math.cos(math.radians(angle))
+            speedy = speed*math.sin(math.radians(angle))
+        self.speedx = speedx
+        self.speedy = speedy
+    
+    def move(self):
+        self.x += self.speedx
+        self.y += self.speedy
+
+    def show(self):
+        pygame.draw.rect(surface, BUBBLES_COLOR,(int(self.x),int(self.y),int(self.z),int(self.w)))
+
+
+class Draw:
+    circles = []
+    squares = []
+    tensor = []
+    screen = surface
+
+    def __init__(self):
+        for x in range(CIRCLE_BUBBLES):
+            self.circles.append(Circle())
+        for x in range(SQUARE_BUBBLES):
+            self.squares.append(Square())
+    
+    def move(self):
+        for circle in self.circles:
+            circle.board_collision()
+
+            for other_circle in self.circles:
+                if circle != other_circle:
+                    circle.circle_collision(other_circle)
+            
+            if self.squares is not None:
+                for square in self.squares:
+                    circle.circle_collision(square)
+            
+            circle.move()
+            circle.show()
+        
+        for square in self.squares:
+            square.board_collision()
+
+            for other_square in self.squares:
+                if square != other_square:
+                    square.square_collision(other_square)
+            
+            if self.circles is not None:
+                for circle in self.circles:
+                    square.square_collision(circle)
+            
+            square.move()
+            square.show()
+
+    # def move_just_square(self):
+    #     for square in self.squares:
+    #         square.board_collision()
+
+    #         for other_square in self.squares:
+    #             if square != other_square:
+    #                 square.square_collision(other_square)
+            
+    #         square.move()
+    #         square.show()
+
+    def show(self):
+        surface.fill(SURFACE_COLOR)
+        self.move()
+        pygame.display.flip()
+        clock.tick(FPS)
+    
+    def save(self, n):
+        file = IMG_PATH + str(n) + '.png'
+        pygame.image.save(surface, file)
+
+    def img2npz(self):
+        # from utils import show_array_as_img
+        for f in os.listdir(IMG_PATH):
+            if f.find(".png") != -1:
+                img = self.img_processing("{}/{}".format(IMG_PATH, f))
+                # show_array_as_img(img, 'gray')
+                self.tensor.append(img)
+
+        np.savez_compressed(NPZ_PATH + "bubbles.npz", self.tensor)
+    
+    def img_processing(self, img_path):
+        img = cv2.imread(img_path, 0) # Convert to grayscale
+        img = cv2.resize(img, (WIDTH, HEIGHT))
+        img = img.astype('float32')
+        img /= 255
+        return img
+
+    def close(self):
+        keystate = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == QUIT or keystate[K_ESCAPE]:
+                pygame.quit(); sys.exit()
+
+
+def run(draw):
+    for i in range(0, NUMBER_OF_DATA):
+    # while True:
+        draw.close()
+        draw.show()
+        draw.save(i+100)
+    draw.img2npz()
+    pygame.quit(); sys.exit()
+
+
+draw = Draw()
+if __name__ == '__main__': run(draw)
---
+++ bubbles/classify.py
@@ -0,0 +1,151 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+import matplotlib.pyplot as plt
+import math
+
+
+WIDTH, HEIGHT = 50, 50
+IMG_PATH = "../../.database/pibic/pygame/img/"
+NPZ_PATH = "../../.database/pibic/pygame/npz/"
+TEST_NPZ_PATH = "../../.database/pibic/pygame/npz_test/"
+MODEL_PATH = "../../.database/pibic/pygame/model/"
+
+
+class BubblesClassifier():
+    model = tf.keras.models.Sequential()
+
+    ACTIONS = [0, 1]
+    MAP_ACTIONS = {0: "quadrado", 1: "circulo"}
+
+    input_shape = (WIDTH, HEIGHT)
+    dim_input = (WIDTH)*(HEIGHT)
+    dim_output = len(ACTIONS)
+    n_hidden_layer = round(math.sqrt((dim_input*dim_output)))
+
+    def __init__(self):
+        self.model.add(
+            layers.SimpleRNN(
+                units=self.n_hidden_layer,
+                input_shape=self.input_shape,
+                activation='tanh',
+                kernel_initializer='random_uniform',
+            )
+        )
+        self.model.add(
+            layers.Dense(
+                self.dim_output,
+                activation='sigmoid'
+            )
+        )
+        self.model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+        print(self.model.summary())
+
+    
+    def prepare_data(self):
+        bubbles = np.load(NPZ_PATH+"bubbles.npz")
+        bubbles_test = np.load(TEST_NPZ_PATH+"bubbles.npz")
+
+        _bubbles = bubbles.f.arr_0
+        _bubbles_test = bubbles_test.f.arr_0
+
+        self.input = _bubbles
+        self.input_test = _bubbles_test
+        
+        squares = np.zeros((int(_bubbles.shape[0]/2),2), dtype=int)
+        circles = np.ones((int(_bubbles.shape[0]/2),2), dtype=int)
+
+        squares[:, [-1]] = 1
+        circles[:, [-1]] = 0
+
+        self.output = np.append(squares, circles, axis=0)
+        self.output_test = np.append(squares, circles, axis=0)
+
+
+    def train(self, epochs, batch_size):
+        self.history = self.model.fit(self.input, self.output, epochs=epochs, batch_size=batch_size)
+
+        scores = self.model.evaluate(self.input_test, self.output_test)
+        print("\n%s: %.2f%%" % (self.model.metrics_names[1], scores[1]*100))
+
+        self.save_model()
+
+
+    def predict(self, observation, reward):
+        actions = self.model.predict(x=self.input)
+        print(actions)
+
+        selected_action = np.argmax(actions)
+
+        # print(selected_action)
+        # print(actions[0, selected_action])
+
+        return selected_action
+    
+
+    def plot_graph(self):
+        plt.plot(self.history.history['accuracy'])
+        # plt.plot(self.history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+        # summarize history for loss
+        plt.plot(self.history.history['loss'])
+        # plt.plot(self.history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+
+
+    def save_model(self):
+        model_json = self.model.to_json()
+
+        with open(MODEL_PATH+"model.json", "w") as json_file:
+            json_file.write(model_json)
+
+        # serialize weights to HDF5
+        self.model.save_weights(MODEL_PATH+"model.h5")
+        print("Saved model to disk")
+
+
+    def load_model(self):
+        self.model = tf.keras.models.load_model(MODEL_PATH+"model.h5")
+
+
+    def show_network(self):
+        from ann_visualizer.visualize import ann_viz;
+
+        # fix random seed for reproducibility
+        np.random.seed(7)
+
+        # load json and create model
+        json_file = open(MODEL_PATH+'model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        _model = tf.keras.models.model_from_json(loaded_model_json)
+        
+        # load weights into new model
+        _model.load_weights(MODEL_PATH+"model.h5")
+        ann_viz(self.model, view=True, title="Artificial Neural network - Model Visualization")
+    
+    def plot_network(self):
+        from keras.utils.vis_utils import plot_model
+        import graphviz
+        from interface import implements, Interface
+
+        path = MODEL_PATH + "model_plot.png"
+        plot_model(self.model, to_file=path, show_shapes=True, show_layer_names=True)
+        print("Saved network graph to disk")
+
+
+a = BubblesClassifier()
+a.prepare_data()
+a.train(epochs=300, batch_size=32)
+a.plot_network()
+a.plot_graph()
+# a.show_network()
